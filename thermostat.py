987b3af122a8b3250ba370f99facd84ccc50bb77
Brock tice <brock@brocktice.com>
2016-05-06 14:51:45 -0600

    Added basic functionality and a test section.

--- thermostat.py
+++ thermostat.py
@@ -1,19 +1,97 @@
 #!/usr/bin/env python
 
-
 import time
 import RPi.GPIO as GPIO
 
+# Use board pin numbers for consistency across models
 GPIO.setmode(GPIO.BOARD)
 
-for pin in [3, 5, 7, 11]:
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
+# Pinout
+# Fan high: Board 3, relay in4
+FANHIGH = 3
 
-for iteration in range(0,10):
-    for pin in [3, 5, 7, 11]:
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(1)
-        GPIO.output(pin, GPIO.HIGH)
-    time.sleep(1)
+# Fan low: Board 5, relay in3
+FANLOW = 5
 
+# Pump: Board 7, Relay in2
+PUMP = 7
+
+# Drain: Board 11, Relay in1
+DRAIN = 11
+
+# Time to prime the pad in seconds
+PRIMETIME=5
+
+# Time to drain in seconds
+DRAINTIME=5
+
+PRIMED=0
+
+def initialize():
+    for pin in [FANHIGH, FANLOW, PUMP, DRAIN]:
+        # note that GPIO.HIGH = relay OFF
+        GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
+        
+def relayOn(pin):
+    GPIO.output(pin, GPIO.LOW)
+
+def relayOff(pin):
+    GPIO.output(pin, GPIO.HIGH)
+
+def primePad():
+    global PRIMED
+
+    print "Priming pad."
+    # fire up the pump for two minues to wet the pads
+    relayOn(PUMP)
+    time.sleep(PRIMETIME)
+    print "Pad primed!"
+    PRIMED=1
+
+def drain():
+    print "Draining."
+    relayOn(DRAIN)
+    time.sleep(DRAINTIME)
+    relayOff(DRAIN)
+    print "Draining complete."
+    
+def runHigh():
+    global PRIMED
+
+    if PRIMED == 0:
+        primePad()
+    print "Running on high"
+    relayOff(FANLOW)
+    relayOn(FANHIGH)
+
+def runLow():
+    global PRIMED
+    if PRIMED == 0:
+        primePad()
+    print "Running on low"
+    relayOff(FANHIGH)
+    relayOn(FANLOW)
+
+def runStop():
+    global PRIMED
+    PRIMED=0
+    relayOff(FANHIGH)
+    relayOff(FANLOW)
+    relayOff(PUMP)
+
+# program logic starts here
+initialize()
+    
+# This will eventually be the main loop once thermistor is in use. For now just do a sample run
+primePad()
+runHigh()
+time.sleep(5)
+runLow()
+time.sleep(5)
+runStop()
+drain()
+
+print "Done running"
+
+# cleanup when done
 GPIO.cleanup()
